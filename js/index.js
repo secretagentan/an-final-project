3df930e8f6d8242c334998ade5a447470eecc365
secretagentan <anguyen021@gmail.com>
2016-09-21 19:02:48 -0700

    submit milestone #3

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 $("document").ready(function(){
 
+// I'm trying to make it so that the background image for each actor expands when you click on the div, but I'm not figuring out how to make it work =(
 	$(".blurb").click(toggleImage);
-	
+
 	function toggleImage(){
 		$(this).addClass("toggle");
 		$(".toggle").slideToggle(); 
 	}
 
-	$("nav li:first-of-type").click(function(){
-		$(this).parent().toggleClass("open");
-	}
+	// $("nav li:first-of-type").click(function(){
+	// 	$(this).parent().toggleClass("open");
+	// }
 
 })
