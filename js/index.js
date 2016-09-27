0893eee07962062b0aff9b09599763befaac5373
secretagentan <anguyen021@gmail.com>
2016-09-26 20:43:49 -0700

    add class "sub-blurb" and fadeIn

--- js/index.js
+++ js/index.js
@@ -1,15 +1,44 @@
+// $("document").ready(function(){
+
+// // I'm trying to make it so that the background image for each actor expands when you click on the div, but I'm not figuring out how to make it work =(
+// 	$(".blurb").click(toggleImage);
+
+// 	function toggleImage(){
+// 		$(this).addClass("toggle");
+// 		$(".toggle").slideToggle(); 
+// 	}
+
+// 	// $("nav li:first-of-type").click(function(){
+// 	// 	$(this).parent().toggleClass("open");
+// 	// }
+
+// })
+
 $("document").ready(function(){
+	$(".sub-blurb").hide();
+	
 
-// I'm trying to make it so that the background image for each actor expands when you click on the div, but I'm not figuring out how to make it work =(
-	$(".blurb").click(toggleImage);
+//**Event Listeners**
+	$("document").ready(main);
 
-	function toggleImage(){
-		$(this).addClass("toggle");
-		$(".toggle").slideToggle(); 
+    $(".blurb").click(toggleImage);
+
+    $("nav li:first-of-type").click(openMenu)
+
+
+//**Functions**
+	function main(){
+		$("h2").hide();
+		$("h2").fadeIn(2000);
 	}
 
-	// $("nav li:first-of-type").click(function(){
-	// 	$(this).parent().toggleClass("open");
-	// }
+    function toggleImage(){
+        $(this).toggleClass("toggle");
+        $(".sub-blurb").show();
+    }
+
+    function openMenu(){
+     $(this).parent().toggleClass("open");
+    }
 
 })
